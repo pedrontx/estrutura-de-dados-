c7aabbfda4ab4d42c214be6fbe893ffb7b689900
pedro <103328669+pedrontx@users.noreply.github.com>
2023-03-13 19:41:00 -0300

    criei um vetor para pegar as notas e usei uma estrutura de repetição

--- vetor_exercicio01.cpp
+++ vetor_exercicio01.cpp
@@ -1,15 +1,24 @@
-// Escreva um algoritimo que solicite as três notas de um aluno e ao final apresente se aprovado ou não 
-
+/* Escreva um algoritimo que solicite a quantidades de avaliações no semestre
+ de um aluno e ao final apresente se aprovado ou não conforme a média aritimetica das N avaliações
+ */
 #include<stdio.h>
 #include<conio.h>
 int main(){
-	float n1, n2, n3, media;
-	printf("informe a 1. nota do aluno: ");
-	scanf("%f",&n1);
-	printf("Informe a 2. nota do aluno: ");
-	scanf("%f",&n2);
-	printf("Informe a 3. nota do aluno: ");
-	scanf("%f",&n3);
-	media = (n1+n2+n3)/3;
-	printf("A media das notas: %.2f", media);
+	
+	float nota[10], soma, media;
+	int i, qtd;
+	soma = 0;
+	printf("informe a quantidade de provas: ");
+	scanf("%i",&qtd);
+
+	for(i=1; i<=qtd; i++){
+		printf("informe a %i. nota do aluno: ",i);
+		scanf("%f",&nota[i]);
+		soma = soma + nota[i]; // isso se chama containe; toda vez que vc ver  essa estrutura uma 
+		//variavel recebe ela mesmo mais alguma coisa; 
+		
+	}
+	media = soma/qtd;
+	printf("\n A media das soma: %.2f", soma);
+	printf("\n A media das notas: %.2f", media);
 }
