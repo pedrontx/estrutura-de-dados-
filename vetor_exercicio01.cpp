793e0e1b290cb3bfab30f0c4671ee568e48ca21b
pedro <103328669+pedrontx@users.noreply.github.com>
2023-03-13 21:43:56 -0300

    usei a posição zero do vertor para fazer a soma e a media aritimetica

--- vetor_exercicio01.cpp
+++ vetor_exercicio01.cpp
@@ -5,20 +5,24 @@
 #include<conio.h>
 int main(){
 	
-	float nota[10], soma, media;
+	float nota[10];
 	int i, qtd;
-	soma = 0;
-	printf("informe a quantidade de provas: ");
-	scanf("%i",&qtd);
-
+	nota[0] = 0;
+	do {
+		printf("\ninforme a quantidade de provas:  ");
+		scanf("%i",&qtd);
+		if(qtd < 1|| qtd > 10)
+			printf("\n Atencaoo!!! Entre 1 e 10!!");	
+	}
+	while(qtd < 1|| qtd > 10);
 	for(i=1; i<=qtd; i++){
 		printf("informe a %i. nota do aluno: ",i);
 		scanf("%f",&nota[i]);
-		soma = soma + nota[i]; // isso se chama containe; toda vez que vc ver  essa estrutura uma 
+		//soma = soma + nota[i]; // isso se chama containe; toda vez que vc ver  essa estrutura uma 
 		//variavel recebe ela mesmo mais alguma coisa; 
+		nota[0]= nota[0]+nota[i]; // a primeira posição do vetor está acumulando a soma das notas
 		
 	}
-	media = soma/qtd;
-	printf("\n A media das soma: %.2f", soma);
-	printf("\n A media das notas: %.2f", media);
+	printf("\n A soma das notas: %.2f", nota[0]);
+	printf("\n A media das notas: %.2f", (nota[0]/qtd));
 }
